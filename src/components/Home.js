d7cb8c8e667db9cfa62d0a9aa7687ecad2d9925e
EmptyBox12 <ataberktumay@gmail.com>
2022-03-01 18:25:03 +0300

    update home-character css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,31 +5,38 @@ export default function Home() {
   const mainCharacters = [
     {
       name: "Erin Solstice",
-      slug:"erin-solstice",
+      slug: "erin-solstice",
       img: "/images/erin.jpg",
       description:
         "Erin Solstice is the main protagonist of The Wandering Inn. She is a [Magical Innkeeper] and a master-class chess player. She first wandered into this world on her way to the bathroom and has been stuck ever since.",
     },
     {
       name: "Pisces Jealnet",
-      slug:"pices-jealnet",
+      slug: "pices-jealnet",
       img: "/images/pisces.png",
       description:
         "Pisces Jealnet is a [Necromancer], and regular customer of The Wandering Inn. He is a Gold-ranked adventurer and member of the Horns of Hammerad.",
     },
     {
       name: "Ceria Springwalker",
-      slug:"ceria-springwalker",
+      slug: "ceria-springwalker",
       img: "/images/ceria.png",
       description:
         "Ceria Springwalker is a half-Elf [Arctic Cryomancer] and captain of the Horns of Hammerad.",
     },
     {
       name: "Pawn",
-      slug:"pawn",
+      slug: "pawn",
       img: "/images/pawn.png",
       description:
         "Pawn is an Individual Worker Antinium that belongs to Liscor's Antinium Hive. He is the first Antinium to become a named individual in the Innverse. Chapters primarily featuring Pawn are often labeled with an L.",
+    },
+    {
+      name: "Flos Reimarch",
+      slug: "flos-reimarch",
+      img: "/images/flos.jpg",
+      description:
+        "Flos Reimarch, also known as Flos, the King of Destruction, is a [King] whose Kingdom, Reim, is located in central Chandrar, beside the Great Desert. He is infamous worldwide, having managed to conquer almost half of the civilized world twenty years ago. The current era of history, A.F., is named for him.",
     },
   ];
   return (
@@ -52,6 +59,7 @@ export default function Home() {
       <div>
         <div className="home-characters">Main Characters</div>
         <hr />
+
         <div className="home-main-characters">
           {mainCharacters.map((character) => {
             return <CharacterCard character={character} />;
